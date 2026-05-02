60b9f7b18d751a004742a46b7075e35b26e22763
mahmudpython <mdmhamud3311@gmail.com>
2026-05-02 06:21:18 +0600

    Add JsonFile and Google Font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Component/Navbar";
+import Footer from "@/Component/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const outfit = Outfit({
   subsets: ["latin"],
 });
 
@@ -21,12 +16,12 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${outfit.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Navbar/>
         {children}
-
+        <Footer/>
         </body>
     </html>
   );
